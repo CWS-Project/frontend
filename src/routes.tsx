50a8f7489b070d9bf3ed49a231e204ece74de7c2
Bhallamudi Sai Narasimha Abhiram <68442741+abhirambsn@users.noreply.github.com>
2024-04-25 14:36:16 +0530

    File based routing added

---
+++ src/routes.tsx
@@ -0,0 +1,29 @@
+import React, { Suspense } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import NotFound from "./errors/NotFound"
+
+const ROUTES = import.meta.glob("/src/pages/**/[a-z[]*.tsx", {eager: true})
+const routes = Object.keys(ROUTES).map((route) => {
+    const path = route.replace(/\/src\/pages|index|\.tsx$/g, '')
+                .replace(/\[\.{3}.+\]/, '*')
+                .replace(/\[(.+)\]/, ':1')
+    
+    // @ts-ignore
+    const Component = ROUTES[route].default
+    return {path, Component: Component}
+})
+
+export const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Suspense fallback={"Loading..."}>
+            <Routes>
+            {routes.map(({path, Component}) => (
+                <Route key={path} path={path} Component={Component} />
+            ))}
+                <Route path="*" Component={NotFound} />
+            </Routes>
+            </Suspense>
+        </BrowserRouter>
+    )
+}
